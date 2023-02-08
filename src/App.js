95cfa9637306cac336a3a0382db7479a8ce21e22
2732ii2 <mo.anas123khan@gmail.com>
2023-02-08 12:37:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Comp/Header/Header';
+import LastSlide from './Comp/LastSlide';
+import LastPart from './Comp/Second_half_comp/LastPart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header Component */}
+      <Header />
+      <LastPart />
+     <LastSlide />
     </div>
   );
 }
